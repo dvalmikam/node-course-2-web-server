55a42845f81f34e5fcdad9f7e9bc2df7081c37dd
Deepthi Valmikam <deepthivalmikam@yahoo.com>
2017-10-04 21:34:15 -0400

    commit

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+hbs.registerPartials(__dirname+'/views/partials');
+app.set('view engine','hbs');
+
+app.use(express.static(__dirname+'/public'));
+
+app.use((req,res,next)=>{
+  var now = new Date().toString();
+  console.log(`${now}:${req.method} ${req.url}`);
+  next();
+});
+
+hbs.registerHelper('getCurrentYEar', ()=>{
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt',(text)=>{
+  return text.toUpperCase();
+});
+app.get('/',(request, response)=>{
+// response.send('Hello Express!');
+response.render('home.hbs',{
+  pageTitle:'Home',
+
+  welcomeMessage:'Hello Express!'
+});
+});
+
+app.get('/about',(request, response)=>{
+response.render('about.hbs',{
+  pageTitle:'About'
+
+});
+});
+
+app.get('/bad',(request, response)=>{
+response.send({
+error:'Error'
+});
+});
+
+app.listen(3000);
